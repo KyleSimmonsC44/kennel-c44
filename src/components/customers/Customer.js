a97f63159c7a990fc8c1be316bf983683767a695
KyleSimmonsC44 <darrensimmons92@gmail.com>
2020-11-23 11:46:53 -0600

    finished thru chapter 4

---
+++ src/components/customers/Customer.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Customer.css"
+
+export const Customer = () => (
+    <section className="customer">
+        <h3 className="customer__name">Hannah Hall</h3>
+        <address>
+            <div>500 Antioch Way</div>
+        </address>
+    </section>
+)
