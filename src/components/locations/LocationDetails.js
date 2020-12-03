d38d8054c85b17f788b7021035eebbdf7cfa5895
KyleSimmonsC44 <darrensimmons92@gmail.com>
2020-12-03 10:35:29 -0600

    complete chapter 18

--- src/components/locations/LocationDetails.js
+++ src/components/locations/LocationDetails.js
@@ -6,16 +6,16 @@ export const LocationDetail = (props) => {
     return (
         <section className="location">
             <h2 className="location__name">{props.location.state.chosenLocation.name}</h2>
+                <p>Currently caring for {
+                    props.location.state.chosenLocation.animals.map(a => a.name).join(", ")
+                }</p>
             <address className="location__address">{props.location.state.chosenLocation.address}</address>
             <div>
                 <h4>Employees</h4>
+                <p>We currently have {`${props.location.state.chosenLocation.employees.length} ${props.location.state.chosenLocation.employees.length === 1 ? "animal lover and trainer:" : "animal lovers and trainers:"}`}</p>
                 { props.location.state.chosenLocation.employees.map(e => e.name).join(", ") }
             </div>
             <div>
-                <h4>Current Residents</h4>
-                {
-                    props.location.state.chosenLocation.animals.map(a => a.name).join(", ")
-                }
             </div>
         </section>
     )
